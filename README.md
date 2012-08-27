04b13ee27652fd2d68d4f7b0dfd3735ebd14de90
Morten Siebuhr <msi@one.com>
2012-08-27 10:14:09 +0200

    Static read/write functions.

--- README.md
+++ README.md
@@ -8,4 +8,20 @@ Reading and writing is done using a cursor and various functions working at the
 cursor. Currently, `.seek()`, `.tell` and index-less versions of
 `{read,write}{U,}Int{8,16,32}` is implemented.
 
+API
+---
+
+    var rb = new RopeBuffer(10); // Same initializers as regular buffers
+	rb.tell; // = 0 - Reading/writing/seeking has happened.
+	rb.writeUInt8(20); // Write something.
+	rb.tell; // = 1 - Just wrote one byte.
+
+	// Use absolute read/writes
+	rb.readUInt8At(0); // = 20; Doesn't move cursor.
+	rb.writeUInt8At(55, 0); // Overwrite first byte.
+
+Most commands for writing/reading integers from buffers have been copied. As
+seen in the example, two versions are exposed; the "plain" one, using a cursor
+for indexing, and an `At`-suffixed version that uses absolute indices.
+
 Also, this is all very α.
--- test/rw-test.js
+++ test/rw-test.js
@@ -27,6 +27,60 @@ vows.describe("Read / Write").addBatch({
                 }
             }
         }
+    },
+    'RopeBuffer(2)': {
+        topic: function () {
+            return new RopeBuffer(2);
+        },
+
+        '.writeInt32BE(0) → Error': function (rb) {
+            assert.throws(
+                function () { rb.writeInt32BE(0); },
+                Error
+            );
+        },
+        '.writeInt32BEAt(0, 0) → Error': function (rb) {
+            assert.throws(
+                function () { rb.writeInt32BEAt(0, 0); },
+                Error
+            );
+        },
+
+        '.writeInt16LEAt(0, 1) → Error': function (rb) {
+            assert.throws(
+                function () { rb.writeInt16LEAt(0, 1); },
+                Error
+            );
+        },
+
+
+        '.writeUInt16BEAt(258, 0)': {
+            topic: function (rb) {
+                rb.writeUInt16BEAt(258, 0);
+                return rb;
+            },
+
+            '.tell → 0': function (rb) {
+                assert.equal(rb.tell, 0);
+            },
+
+            '.readUInt16BEAt(0) → 258': function (rb) {
+                assert.equal(rb.readUInt16BEAt(0), 258);
+            },
+
+            '.readUInt8At(0) → 1': function (rb) {
+                assert.equal(rb.readUInt8At(0), 1);
+            },
+            '.readUInt8At(1) → 2': function (rb) {
+                assert.equal(rb.readUInt8At(1), 2);
+            },
+            '.readUInt8At(2) → Error': function (rb) {
+                assert.throws(
+                    function () { rb.readUInt8At(2); },
+                    Error
+                );
+            }
+        }
     }
 })['export'](module);
 
